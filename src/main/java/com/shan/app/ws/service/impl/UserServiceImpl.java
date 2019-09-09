b9272c5fb0839cfda038c421f789163d15e4b9d1
TimothySjiang <shjiang@ucdavis.edu>
2019-09-09 11:04:26 -0700

    loginin

--- src/main/java/com/shan/app/ws/service/impl/UserServiceImpl.java
+++ src/main/java/com/shan/app/ws/service/impl/UserServiceImpl.java
@@ -1,18 +1,36 @@
 package com.shan.app.ws.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.shan.app.ws.UserRepository;
+import com.shan.app.ws.exceptions.UserServiceException;
+import com.shan.app.ws.io.entity.PasswordResetTokenEntity;
 import com.shan.app.ws.io.entity.UserEntity;
+import com.shan.app.ws.io.repository.PasswordResetTokenRepository;
+import com.shan.app.ws.io.repository.UserRepository;
 import com.shan.app.ws.service.UserService;
+import com.shan.app.ws.shared.AmazonSES;
 import com.shan.app.ws.shared.Utils;
+import com.shan.app.ws.shared.dto.AddressDTO;
 import com.shan.app.ws.shared.dto.UserDto;
+import com.shan.app.ws.ui.model.response.ErrorMessages;
 
-
+@Service
 public class UserServiceImpl implements UserService {
+
 	@Autowired
 	UserRepository userRepository;
 
@@ -27,7 +45,7 @@ public class UserServiceImpl implements UserService {
 	
 	@Autowired
     AmazonSES amazonSES;
-
+ 
 	@Override
 	public UserDto createUser(UserDto user) {
 
@@ -76,45 +94,161 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		UserEntity userEntity = userRepository.findByEmail(email);
+
+		if (userEntity == null)
+			throw new UsernameNotFoundException(email);
+		
+		return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), 
+				userEntity.getEmailVerificationStatus(),
+				true, true,
+				true, new ArrayList<>());
+
+		//return new User(userEntity.getEmail(), userEntity.getEncryptedPassword(), new ArrayList<>());
+	}
+
+	@Override
 	public UserDto getUserByUserId(String userId) {
-		// TODO Auto-generated method stub
-		return null;
+		UserDto returnValue = new UserDto();
+		UserEntity userEntity = userRepository.findByUserId(userId);
+
+		if (userEntity == null)
+			throw new UsernameNotFoundException("User with ID: " + userId + " not found");
+
+		BeanUtils.copyProperties(userEntity, returnValue);
+
+		return returnValue;
 	}
 
 	@Override
 	public UserDto updateUser(String userId, UserDto user) {
-		// TODO Auto-generated method stub
-		return null;
+		UserDto returnValue = new UserDto();
+
+		UserEntity userEntity = userRepository.findByUserId(userId);
+
+		if (userEntity == null)
+			throw new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage());
+
+		userEntity.setFirstName(user.getFirstName());
+		userEntity.setLastName(user.getLastName());
+
+		UserEntity updatedUserDetails = userRepository.save(userEntity);
+		returnValue = new ModelMapper().map(updatedUserDetails, UserDto.class);
+
+		return returnValue;
 	}
 
+	@Transactional
 	@Override
 	public void deleteUser(String userId) {
-		// TODO Auto-generated method stub
+		UserEntity userEntity = userRepository.findByUserId(userId);
+
+		if (userEntity == null)
+			throw new UserServiceException(ErrorMessages.NO_RECORD_FOUND.getErrorMessage());
+
+		userRepository.delete(userEntity);
 
 	}
 
 	@Override
 	public List<UserDto> getUsers(int page, int limit) {
-		// TODO Auto-generated method stub
-		return null;
+		List<UserDto> returnValue = new ArrayList<>();
+		
+		if(page>0) page = page-1;
+		
+		Pageable pageableRequest = PageRequest.of(page, limit);
+		
+		Page<UserEntity> usersPage = userRepository.findAll(pageableRequest);
+		List<UserEntity> users = usersPage.getContent();
+		
+        for (UserEntity userEntity : users) {
+            UserDto userDto = new UserDto();
+            BeanUtils.copyProperties(userEntity, userDto);
+            returnValue.add(userDto);
+        }
+		
+		return returnValue;
 	}
 
 	@Override
 	public boolean verifyEmailToken(String token) {
-		// TODO Auto-generated method stub
-		return false;
+	    boolean returnValue = false;
+
+        // Find user by token
+        UserEntity userEntity = userRepository.findUserByEmailVerificationToken(token);
+
+        if (userEntity != null) {
+            boolean hastokenExpired = Utils.hasTokenExpired(token);
+            if (!hastokenExpired) {
+                userEntity.setEmailVerificationToken(null);
+                userEntity.setEmailVerificationStatus(Boolean.TRUE);
+                userRepository.save(userEntity);
+                returnValue = true;
+            }
+        }
+
+        return returnValue;
 	}
 
 	@Override
 	public boolean requestPasswordReset(String email) {
-		// TODO Auto-generated method stub
-		return false;
+		
+        boolean returnValue = false;
+        
+        UserEntity userEntity = userRepository.findByEmail(email);
+
+        if (userEntity == null) {
+            return returnValue;
+        }
+        
+        String token = new Utils().generatePasswordResetToken(userEntity.getUserId());
+        
+        PasswordResetTokenEntity passwordResetTokenEntity = new PasswordResetTokenEntity();
+        passwordResetTokenEntity.setToken(token);
+        passwordResetTokenEntity.setUserDetails(userEntity);
+        passwordResetTokenRepository.save(passwordResetTokenEntity);
+        
+        returnValue = new AmazonSES().sendPasswordResetRequest(
+                userEntity.getFirstName(), 
+                userEntity.getEmail(),
+                token);
+        
+		return returnValue;
 	}
 
 	@Override
 	public boolean resetPassword(String token, String password) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+        boolean returnValue = false;
+        
+        if( Utils.hasTokenExpired(token) )
+        {
+            return returnValue;
+        }
+ 
+        PasswordResetTokenEntity passwordResetTokenEntity = passwordResetTokenRepository.findByToken(token);
 
+        if (passwordResetTokenEntity == null) {
+            return returnValue;
+        }
+
+        // Prepare new password
+        String encodedPassword = bCryptPasswordEncoder.encode(password);
+        
+        // Update User password in database
+        UserEntity userEntity = passwordResetTokenEntity.getUserDetails();
+        userEntity.setEncryptedPassword(encodedPassword);
+        UserEntity savedUserEntity = userRepository.save(userEntity);
+ 
+        // Verify if password was saved successfully
+        if (savedUserEntity != null && savedUserEntity.getEncryptedPassword().equalsIgnoreCase(encodedPassword)) {
+            returnValue = true;
+        }
+   
+        // Remove Password Reset token from database
+        passwordResetTokenRepository.delete(passwordResetTokenEntity);
+        
+        return returnValue;
+	}
+	
 }
